b79c145f9bf2c5d125585ee339cd962fa38b1e21
Nikhil Bhor <129065751+NikhilBBhor@users.noreply.github.com>
2025-06-03 03:27:31 +0000

    Learned tokenization

--- What is GEN AI - Introduction/tokenization.py
+++ What is GEN AI - Introduction/tokenization.py
@@ -2,6 +2,17 @@
 
 import tiktoken
 
-encder = tiktoken.encoding_for_model('gpt-4o')
+# understanding vocab size
+encoder = tiktoken.encoding_for_model('gpt-4o')
+print("Vocab Size:", encoder.n_vocab)  # --> 200019
 
-print("Vocab Size: ", encder.n_vocab)  # --> 200019
+# encoding text to tokens
+text = "The cat sat on the mat"
+tokens = encoder.encode(text)
+print("Tokens:", tokens)  # --> [976, 9059, 10139, 402, 290, 2450]
+
+# decoding tokens to text
+my_tokens = [976, 9059, 10139, 402, 290, 2450]
+decoded_text = encoder.decode(my_tokens)
+print("Decoded tokens:", decoded_text)
+
