ac9a6b8e505e24fb68d2862143b78281875d6ca9
Nikhil Bhor <129065751+NikhilBBhor@users.noreply.github.com>
2025-06-19 13:45:54 +0000

    Learned RAG

--- 06 RAG/01_rag.py
+++ 06 RAG/01_rag.py
@@ -1,44 +1,54 @@
-from langchain_community.document_loaders import PyPDFLoader # type: ignore
-from langchain_text_splitters import RecursiveCharacterTextSplitter # type: ignore
+from pathlib import Path
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_qdrant import QdrantVectorStore
+from dotenv import load_dotenv
 
-file_path = "/workspaces/Concepts-to-deploy-GEN-AI-projects/06 RAG/nodejs.pdf"
-loader = PyPDFLoader(file_path) 
-
-docs = loader.load() # by default it loads the pdf each page saparate
-
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=200
-)
-
-split_docs = text_splitter.split_documents(documents=docs)
+load_dotenv()
 
 embedder = OpenAIEmbeddings(
     model="text-embedding-3-large"                              
 )
 
-############ Need only one time, while creating embeddings #######
-# vectore_store = QdrantVectorStore.from_documents(
-#     documents=[],
-#     collection_name="learning_langchain",
-#     url="http://localhost:6333",
-#     embedding=embedder,
-# )
-#
-# vectore_store.add_documents(documents=split_docs)
-#
-# print("Injection Done")
+def chunking(file_path: str):
 
-retriever = QdrantVectorStore.from_existing_collection(
-    collection_name="learning_langchain",
-    url="http://localhost:6333",
-    embedding=embedder,
-)
+    loader = PyPDFLoader(file_path) 
+    docs = loader.load() # by default it loads(chunks) the pdf each page saparately
 
-relevant_chunks = retriever.similarity_search(
-    query="What is FS Module?"
-)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=2000,
+        chunk_overlap=200
+    )
 
-print("Relevat chunks:", relevant_chunks)
+    split_docs = text_splitter.split_documents(documents=docs)
+
+    vectore_store = QdrantVectorStore.from_documents(
+        documents=[],
+        collection_name="learning_langchain",
+        url="http://localhost:6333",
+        embedding=embedder,
+    )
+
+    vectore_store.add_documents(documents=split_docs)
+
+    print("Injection Done")
+
+def retrieve(query: str):
+    
+    retriever = QdrantVectorStore.from_existing_collection(
+        collection_name="learning_langchain",
+        url="http://localhost:6333",
+        embedding=embedder,
+    )
+
+    relevant_chunks = retriever.similarity_search(
+        query=query
+    )
+
+    print("Relevat chunks:", relevant_chunks)
+
+# file_path = Path(__file__).parent / "nodejs.pdf"
+# chunking(file_path)
+
+retrieve("What is FS Module?")
